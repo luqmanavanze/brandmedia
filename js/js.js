e7d98d856a6681d03bad9642f4af6048d9e98550
luqman <luqman.kunduthodi@avanzegroup.com>
2024-04-26 07:10:49 +0530

    updates

--- js/js.js
+++ js/js.js
@@ -13,6 +13,31 @@ $(document).ready(function(){
         });
     });
 
+    $(".col-6.col-lg-3.col-xl-2").click(function(){
+        $(".eachmodal").modal('show');
+        const Heading = $(this).children(".custom-cards").children(".card-body").children(".card-title").html();
+        const Stars = $(this).children(".custom-cards").children(".card-body").children("ul").html();
+        const copyImg = $(this).children(".custom-cards").children(".bg-image").children(".img-fluid").attr("src");
+        const Desc = $(this).children(".custom-cards").children(".card-body").children(".mb-2").children("b").html();
+        const Size = $(this).children(".custom-cards").children(".card-body").children(".card-text").html();
+        $('.img-copy').html('<img src="'+copyImg+'" alt="" class="img-fluid" />');
+        $('.copymodeltitle').html(Heading);
+        $('.copystars').html(Stars);
+        console.log(Desc);
+        if(Desc === undefined){
+            $('.copydesc').html('');
+        }else{
+            $('.copydesc').html(Desc);
+        }
+
+        if(Size === undefined){
+            $('.copysize').html('')
+        }else{
+            $('.copysize').html(Size)
+        }
+
+        
+    });
 
 
 });
